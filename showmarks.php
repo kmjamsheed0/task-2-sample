2d00168327665a3d30748457ed16614f2501bd44
Zennode <zennode@Zennodes-MacBook-Air.local>
2021-09-24 18:39:00 +0530

    Day 5 works

--- showmarks.php
+++ showmarks.php
@@ -6,26 +6,43 @@ $database = "student";
 $regno = $_POST['regno'];
 $db_handle = new mysqli($server,$user_name,$password);
 if(!$db_handle){
-die("Could not Connect: " . mysql_error());
-}
-$db_found= mysqli_select_db($database, $db_handle);
+	die("Could not Connect: " . mysql_error());
+	}
+$db_found= mysqli_select_db($db_handle,$database);
 if($db_found){
-$SQL= "SELECT * FROM mark WHERE regno='".$regno."'";
-$result_set = mysqli_query($SQL);
-$record=mysqli_fetch_array($result_set);
-echo "<BR>MARK LIST";
-echo "<BR>";
-echo "<BR>Reg. no.:".$record['regno'];
-echo "<BR>Name  :".$record['sname'];
-echo "<BR>Dept  :".$record['sgroup'];
-echo "<BR>Mark1 :".$record['mark1'];
-echo "<BR>Mark2  :".$record['mark2'];
-echo "<BR>Mark3  :".$record['mark3'];
-echo "<BR>Result   :".compute_result($record['mark1'],$record['mark2'],$record['mark3']);
-}
+		$SQL= "SELECT * FROM mark WHERE regno='".$regno."'";
+		/*$SQL= "SELECT * FROM mark WHERE regno=?";*/
+		/*$SQL->bind_param("s", $regno);
+		$SQL->execute();
+		$result_set = $SQL->get_result();*/
+		$result_set = mysqli_query($db_handle, $SQL);
+		/*$result_set=$db_handle->query($SQL);*/
+		/*$record=mysql_fetch_array($record);*/
+		/*$record=$result_set->fetch_assoc();*/
+	
+	$record = mysqli_fetch_array($result_set, MYSQLI_ASSOC);
+	if($record)
+	{
+		echo " fetched";
+	}
+	else
+	{
+		echo " not fetched";
+	}
+	echo "<BR>MARK LIST";
+	echo "<BR>";
+	echo "<BR>Reg. no.:".$record['regno'];
+	echo "<BR>Name  :".$record['sname'];
+	echo "<BR>Dept  :".$record['sgroup'];
+	echo "<BR>Mark1 :".$record['mark1'];
+	echo "<BR>Mark2  :".$record['mark2'];
+	echo "<BR>Mark3  :".$record['mark3'];
+	echo "<BR>Result   :".compute_result($record['mark1'],$record['mark2'],$record['mark3']);
+	}
 else
-{echo"<BR>Database not found";}
+	{echo"<BR>student Database not found ";}
 mysqli_close($db_handle);
+
 function compute_result($m1,$m2,$m3)
 {	$tmarks=$m1 + $m2+$m3;
 	if(($m1<45)||($m2<45)||($m3<45))
